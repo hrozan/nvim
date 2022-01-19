92b6f8d3e248adb57d33577c4978f0a7f3ef7bec
hrozan <higorb.rozan@gmail.com>
2022-01-19 01:57:34 -0300

    feat: add lualine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -33,6 +33,12 @@ return packer.startup(function(use)
     run = ':TSUpdate',
     config = function() require('plugins.treesitter') end
   }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function() require('plugins.lualine') end  
+  }
   
   -- Gitsigns
   use {
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,9 @@
+local lualine = require('lualine') 
+
+lualine.setup {
+  options = {
+    theme = 'onedark',
+    section_separators = '', 
+    component_separators = ''
+  }
+}
