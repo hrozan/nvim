3021e8ac3b798a3a3fc499c4333bc0fc6869a22c
hrozan <higorb.rozan@gmail.com>
2022-03-08 14:51:25 -0300

    fix: nvim tree config

--- lua/hrozan/tree.lua
+++ lua/hrozan/tree.lua
@@ -6,17 +6,23 @@ g.nvim_tree_add_trailing = 0
 g.nvim_tree_git_hl = 0
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_indent_markers = 0
-g.nvim_tree_quit_on_open = 1
 g.nvim_tree_root_folder_modifier = table.concat {
     ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??",
 }
 
-g.nvim_tree_window_picker_exclude = {
-    filetype = {"notify", "packer", "qf"},
-    buftype = {"terminal"},
-}
-
+g.nvim_tree_window_picker_exclude = 
 tree.setup {
+    actions = {
+      open_file = {
+        window_picker = {
+          exclude = {
+            filetype = {"notify", "packer", "qf"},
+            buftype = {"terminal"},
+          }
+        },
+        quit_on_open = true,
+      }
+    },
     filters = {dotfiles = false},
     ignore_ft_on_setup = {"dashboard"},
     open_on_tab = false,
