2135bff4f114a1d5c07dbeb3c9117dffc5e3abef
Higor Rozan <higorb.rozan@gmail.com>
2022-07-18 23:54:00 -0300

    feat: add settings

--- init.lua
+++ init.lua
@@ -1,19 +1,6 @@
 -- Author: Higor Rozan
 -- higorb.rozan@hotmail.com
+
 require("plugins")
 require("mappings")
-
-vim.g.material_style = "deep ocean"
-vim.o.guifont = "JetBrainsMonoMedium Nerd Font:h13"
-vim.o.number = true
-vim.o.numberwidth = 2
-vim.o.relativenumber = true
-vim.o.clipboard = "unnamedplus"
-vim.o.swapfile = false
-vim.o.termguicolors = true
-vim.o.shiftwidth = 4
-vim.o.smartindent = true
-vim.o.mouse = "a"
-vim.o.undofile = true
-
-vim.cmd("colorscheme material")
+require("settings")
---
+++ lua/settings.lua
@@ -0,0 +1,14 @@
+vim.g.material_style = "deep ocean"
+vim.o.guifont = "JetBrainsMonoMedium Nerd Font:h13"
+vim.o.number = true
+vim.o.numberwidth = 2
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+vim.o.swapfile = false
+vim.o.termguicolors = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.mouse = "a"
+vim.o.undofile = true
+
+vim.cmd("colorscheme material")
