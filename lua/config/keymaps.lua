e378cf9d16379b145f09f54e8140d8c33a65153b
Higor Rozan <higorb.rozan@gmail.com>
2023-03-21 00:37:50 -0300

    add dap and dapui

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,27 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<cr>')
+
+vim.keymap.set('n', '<F5>', function()
+  require('dap').toggle_breakpoint()
+end, { desc = 'Toggle Breakpoint' })
+
+vim.keymap.set('n', '<F9>', function()
+  require('dap').continue()
+end, { desc = 'Continue' })
+
+vim.keymap.set('n', '<F8>', function()
+  require('dap').step_over()
+end, { desc = 'Step Over' })
+
+vim.keymap.set('n', '<F7>', function()
+  require('dap').step_into()
+end, { desc = 'Step Into' })
+
+vim.keymap.set('n', '<F6>', function()
+  require('dap').step_out()
+end, { desc = 'Step Out' })
+
+vim.keymap.set('n', '<F2>', function()
+  require('dapui').toggle()
+end, { desc = 'Toggle Debugger UI' })
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -16,4 +16,45 @@ return {
     version = '*',
     config = true,
   },
+  {
+    'folke/tokyonight.nvim',
+    opts = {
+      style = 'night',
+    },
+  },
+  {
+    'simrat39/rust-tools.nvim',
+    config = function()
+      local rt = require 'rust-tools'
+
+      rt.setup {
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      }
+    end,
+  },
+  { 'mfussenegger/nvim-dap' },
+  {
+    'rcarriga/nvim-dap-ui',
+    config = function()
+      local dap, dapui = require 'dap', require 'dapui'
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+
+      dapui.setup()
+    end,
+  },
 }
