3efe0676430a9e02b7ee8fcfb15034a9010f122c
Higor Rozan <higorb.rozan@gmail.com>
2023-03-24 10:42:26 -0300

    add go lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<cr>')
+vim.keymap.set('n', '<A-2>', '<cmd>RustDebuggables<cr>')
 
 vim.keymap.set('n', '<F5>', function()
   require('dap').toggle_breakpoint()
@@ -27,6 +28,6 @@ vim.keymap.set('n', '<F2>', function()
   require('dapui').toggle()
 end, { desc = 'Toggle Debugger UI' })
 
-vim.keymap.set('n', '<leader>cs', function()
-  require('rust-tools').hover_actions.hover_actions()
-end, { desc = 'Rust Tools Code Action' })
+vim.keymap.set('n', '<F10>', function()
+  require('dap').run_last()
+end, { desc = 'Run Last' })
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,18 @@
+return {
+  { 'simrat39/rust-tools.nvim', config = true },
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+}
