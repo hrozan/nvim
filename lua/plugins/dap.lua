b531d2380d6f49811f3c7fc6466d7fed42b9bdc3
Higor Bassi <higorb.rozan@lambda3.com.br>
2023-03-24 17:25:06 -0300

    update node dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,11 +3,14 @@ return {
     'mfussenegger/nvim-dap',
     config = function()
       local dap = require 'dap'
+
       dap.adapters.node2 = {
         type = 'executable',
         command = 'node',
+        args = { os.getenv 'HOME' .. '/.dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
       }
-      dap.configurations.javascript = {
+
+      dap.configurations.typescript = {
         {
           name = 'Launch',
           type = 'node2',
