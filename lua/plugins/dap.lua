42be9b885c45bc6bf74bae4b3abb8cca9e46a607
Higor Rozan <higorb.rozan@gmail.com>
2023-03-27 09:31:59 -0300

    feat: node dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,32 +1,62 @@
 return {
+  { 'mxsdev/nvim-dap-vscode-js' },
   {
     'mfussenegger/nvim-dap',
+    keys = {
+      { '<A-9>', '<cmd>DapContinue<cr>', desc = 'Dap Continue' },
+    },
     config = function()
       local dap = require 'dap'
 
-      dap.adapters.node2 = {
-        type = 'executable',
-        command = 'node',
-        args = { os.getenv 'HOME' .. '/.dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
+      -- setup adapters
+      require('dap-vscode-js').setup {
+        debugger_path = vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter',
+        debugger_cmd = { 'js-debug-adapter' },
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
       }
 
+      -- custom adapter for running tasks before starting debug
+      local custom_adapter = 'pwa-node-custom'
+      dap.adapters[custom_adapter] = function(cb, config)
+        if config.preLaunchTask then
+          local async = require 'plenary.async'
+          local notify = require('notify').async
+
+          async.run(function()
+            ---@diagnostic disable-next-line: missing-parameter
+            notify('Running [' .. config.preLaunchTask .. ']').events.close()
+          end, function()
+            vim.fn.system(config.preLaunchTask)
+            config.type = 'pwa-node'
+            dap.run(config)
+          end)
+        end
+      end
+
+      -- language config
       dap.configurations.typescript = {
         {
-          name = 'Launch',
-          type = 'node2',
+          type = 'pwa-node',
           request = 'launch',
+          name = 'Launch file',
           program = '${file}',
-          cwd = vim.fn.getcwd(),
-          sourceMaps = true,
-          protocol = 'inspector',
-          console = 'integratedTerminal',
+          cwd = '${workspaceFolder}',
+          runtimeExecutable = './node_modules/.bin/tsx',
         },
         {
-          -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-          name = 'Attach to process',
-          type = 'node2',
-          request = 'attach',
-          processId = require('dap.utils').pick_process,
+          type = 'pwa-node',
+          runtimeExecutable = 'tsx',
+          request = 'launch',
+          name = 'Debug Jest Tests',
+          -- trace = true, -- include debugger info
+          runtimeArgs = {
+            './node_modules/jest/bin/jest.js',
+            '--runInBand',
+          },
+          rootPath = '${workspaceFolder}',
+          cwd = '${workspaceFolder}',
+          console = 'integratedTerminal',
+          internalConsoleOptions = 'neverOpen',
         },
       }
     end,
