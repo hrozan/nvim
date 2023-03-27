fdcd46a49925ce2799529a3501ad8721474a698d
Higor Bassi <higorb.rozan@lambda3.com.br>
2023-03-27 15:16:07 -0300

    change neotree side

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,4 +4,14 @@ return {
     version = '*',
     config = true,
   },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    opts = {
+      close_if_last_window = true,
+      window = {
+        position = 'right',
+        width = 45,
+      },
+    },
+  },
 }
