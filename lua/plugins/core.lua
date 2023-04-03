fbde0f7a3c5aa3c8cc07b097e2438cb551582256
Higor Bassi <higorb.rozan@lambda3.com.br>
2023-04-03 10:01:43 -0300

    map neotree to <A-1>

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,7 +5,22 @@ return {
     config = true,
   },
   {
+    'rcarriga/nvim-notify',
+    keys = {
+      {
+        '<A-l>',
+        function()
+          require('notify').dismiss { silent = true, pending = true }
+        end,
+        desc = 'Delete all Notifications',
+      },
+    },
+  },
+  {
     'nvim-neo-tree/neo-tree.nvim',
+    keys = {
+      { '<A-1>', '<cmd>Neotree toggle<cr>', desc = 'Neotree Toggle' },
+    },
     opts = {
       close_if_last_window = true,
       window = {
