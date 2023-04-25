5744586a208d7eeb653476789c8a2bc6145bcea1
Higor Rozan <higorb.rozan@gmail.com>
2023-04-25 14:52:28 -0300

    feat: add python neotest

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -2,12 +2,14 @@ return {
   { 'antoinemadec/FixCursorHold.nvim', config = function() vim.cmd 'let g:cursorhold_updatetime = 100' end },
   { 'nvim-neotest/neotest-jest' },
   { 'rouge8/neotest-rust' },
+  { 'nvim-neotest/neotest-python' },
   {
     'nvim-neotest/neotest',
     config = function()
       require('neotest').setup {
         adapters = {
           require 'neotest-jest' {},
+          require 'neotest-python' {},
           require 'neotest-rust',
         },
       }
